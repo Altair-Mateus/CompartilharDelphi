a9f3fa3987100527d10c11b19116c18b1a1de5d5
Altair-Mateus <taicoalencastro@gmail.com>
2025-11-17 20:52:38 -0300

    feat: AJuste no caminho e organização das pastas do comboBoxHelper

--- Helpers/TComboBox/src/DemoComboBoxHelper/ExemploComboBoxHelper.dpr
+++ Helpers/TComboBox/src/DemoComboBoxHelper/ExemploComboBoxHelper.dpr
@@ -2,14 +2,15 @@ program ExemploComboBoxHelper;
 
 uses
   Vcl.Forms,
-  fPrincipal in 'fPrincipal.pas' {frmPrincipal},
-  uComboBoxHelper in 'uComboBoxHelper.pas';
+  fPrincipal in 'src\view\fPrincipal.pas' {frmPrincipal},
+  uComboBoxHelper in '..\Classes\uComboBoxHelper.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+
   Application.CreateForm(TfrmPrincipal, frmPrincipal);
   Application.Run;
 end.
